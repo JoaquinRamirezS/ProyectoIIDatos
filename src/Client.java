e6e21a75aa09170681a16b027ccb8f579c1f8d84
JoaquinRamirezS <jramirezsequeira@gmail.com>
2023-10-14 19:53:45 -0600

    FeatureConnectionClient/Server

--- src/Client.java
+++ src/Client.java
@@ -1,61 +1,122 @@
 import javax.swing.*;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionListener;
-import java.awt.GridBagConstraints;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.awt.event.ActionEvent;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class Client extends JFrame {
     Font customFont = new Font("Arial", Font.BOLD, 18);
     JTextField tfname, tfexpression;
+    DataInputStream input;
+    DataOutputStream output;
+    Socket SocketClient;
 
     public void initialize() {
+        //Main Panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(new Color(80, 153, 193 ));
+        mainPanel.setLayout(null);
+
+        //Label Date
+        JLabel lbdate = new JLabel("Fecha: ");
+        lbdate.setFont(customFont);
+        lbdate.setBounds(50, 20, 100, 30);
+        mainPanel.add(lbdate);
+        
+        //Date of the system
+        Calendar date = new GregorianCalendar();
+        String year = Integer.toString(date.get(Calendar.YEAR));
+        String month = Integer.toString(date.get(Calendar.MONTH)+1);
+        String day = Integer.toString(date.get(Calendar.DAY_OF_MONTH));
+        String cdate = day + "/" + month + "/" + year;
+        
+        //Put the complete date on a label
+        JLabel pdate = new JLabel(cdate);
+        pdate.setFont(customFont);
+        pdate.setBounds(120, 20, 200, 30);
+        mainPanel.add(pdate);
+
         //Label Name
         JLabel lbname = new JLabel("Ingrese su nombre");
         lbname.setFont(customFont);
+        lbname.setBounds(50, 70, 200, 30);
+        mainPanel.add(lbname);
 
-        //TextField for name 
+        //TextField for name
         tfname = new JTextField(20); 
         tfname.setFont(customFont);
         tfname.setBackground(new Color (197, 198, 208));
+        tfname.setBounds(250, 70, 200, 30);
+        mainPanel.add(tfname);
 
         //Label for Expression
-        JLabel lbexpression = new JLabel("Ingrese la expresion que desee resolver:");
+        JLabel lbexpression = new JLabel("Ingrese una expresion:");
         lbexpression.setFont(customFont);
+        lbexpression.setBounds(50, 120, 300, 30);
+        mainPanel.add(lbexpression);
 
         //TextField Expression
         tfexpression = new JTextField(20); 
         tfexpression.setFont(customFont);
         tfexpression.setBackground(new Color (197, 198, 208));
+        tfexpression.setBounds(50, 150, 400, 30);
+        mainPanel.add(tfexpression);
 
         //TextArea to show the result of the expression
-        JTextArea taresult = new JTextArea(10, 20); 
+        JTextArea taresult = new JTextArea(10, 40); 
         taresult.setFont(customFont);
         taresult.setBackground(new Color (197, 198, 208));
+        taresult.setBounds(50, 190, 400, 200);
+        mainPanel.add(taresult);
 
-        //Button Acept
-        JButton bresult = new JButton();
+        //Buttom "Aceptar"
+        JButton bresult = new JButton("Aceptar");
         bresult.setFont(customFont);
-        bresult.setText("Aceptar");
-        bresult.setPreferredSize(new Dimension (150, 40));
+        bresult.setBounds(90, 400, 150, 40);
+        mainPanel.add(bresult);
         bresult.addActionListener(new ActionListener() {
             @Override 
             public void actionPerformed(ActionEvent e) {
-                String resultexp = tfexpression.getText();
-                taresult.setText("Resultado: " + resultexp);
+                //Se obtienen los valores ingresados
+                String name = tfname.getText();
+                String expresion = tfexpression.getText();
+
+                try {
+                    Socket socket = new Socket("127.0.0.1", 1800);
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+        
+                    //Send all the dates to the server
+                    output.writeUTF(name);
+                    output.writeUTF(expresion);
+
+                    System.out.println("Datos enviados al servidor.");
+
+                    //Show the result from de server
+                    String result = input.readUTF();
+                    taresult.setText("\nResultado: " + result);
+
+                    socket.close();
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    System.out.println("Error al enviar datos al servidor: " + ex.getMessage());
+                }
             }
         });
 
-        //Buttom Clean
-        JButton bclean = new JButton();
+        //Buttom "Limpiar"
+        JButton bclean = new JButton("Limpiar");
         bclean.setFont(customFont);
-        bclean.setText("Limpiar");
-        bclean.setPreferredSize(new Dimension (150, 40));
+        bclean.setBounds(250, 400, 150, 40);
+        mainPanel.add(bclean);
         bclean.addActionListener(new ActionListener() {
             @Override 
             public void actionPerformed(ActionEvent e) {
@@ -64,42 +125,19 @@ public class Client extends JFrame {
             }
         });
 
-        //Buttoms position
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.add(bresult);  
-        buttonPanel.add(bclean); 
-
-        // Main Panel
-        JPanel mainPanel = new JPanel();
-        mainPanel.setOpaque(true);
-        mainPanel.setLayout(new GridBagLayout());
-        mainPanel.setBackground(new Color(80, 153, 193));
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.insets = new Insets(10, 10, 10, 10);
-        mainPanel.add(lbname, gbc);
-        gbc.gridy = 1;
-        mainPanel.add(tfname, gbc);
-        gbc.gridy = 2;
-        mainPanel.add(lbexpression, gbc);
-        gbc.gridy = 3;
-        mainPanel.add(tfexpression, gbc);
-        gbc.gridy = 4;
-        mainPanel.add(taresult, gbc);
-        gbc.gridy=5;
-        mainPanel.add(buttonPanel, gbc); 
-
         add(mainPanel);
-
-        //Define the window
+        
+        // Define the window
         setTitle("Calculator");
         setSize(500, 550);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
     }
 
     public static void main(String[] args) {
-        Client frame = new Client();
+        Client frame = new Client(); 
         frame.initialize();
+      
     }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,3 +1,70 @@
-public class Servidor {
-                    
+import javax.swing.*;
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor extends JFrame {
+    private JTextArea textArea;
+
+    public Servidor() {
+        setTitle("Servidor");
+        setSize(500, 550);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public void startServer() {
+        int puerto = 1800;
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(puerto);
+            textArea.append("El servidor está escuchando en el puerto " + puerto + "\n");
+
+            while (true) {
+                Socket socketCliente = serverSocket.accept();
+                textArea.append("Cliente conectado desde " + socketCliente.getInetAddress() + "\n");
+
+                DataInputStream input = new DataInputStream(socketCliente.getInputStream());
+                DataOutputStream output = new DataOutputStream(socketCliente.getOutputStream());
+
+                String nombre = input.readUTF();
+                String expresion = input.readUTF();
+
+                textArea.append("Nombre: " + nombre + "\n");
+                textArea.append("Expresión: " + expresion + "\n");
+
+                String resultado =  expresion;
+                output.writeUTF(resultado);
+
+                socketCliente.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Servidor server = new Servidor();
+            server.setVisible(true);
+            server.startServer();
+        });
+    }
 }
